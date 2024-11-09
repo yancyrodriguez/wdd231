9a93967a0d692cb269d1649814d338fb602a8ead
yancyrodriguez <rod22047@byui.edu>
2024-11-09 10:02:11 -0700

    script added

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,4 @@ const currentYear = new Date().getFullYear();
 document.getElementById('currentyear').textContent = currentYear;
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = lastModified
+
