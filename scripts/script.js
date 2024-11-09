22246d35c119fe36734a1c6f7aa238ef93c82d09
yancyrodriguez <rod22047@byui.edu>
2024-11-09 12:52:19 -0700

    changes

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,10 @@ document.getElementById('currentyear').textContent = currentYear;
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = lastModified
 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
