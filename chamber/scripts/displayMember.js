0cfdbc2f0878260240aced826de30f3219afce0b
yancyrodriguez <rod22047@byui.edu>
2024-11-14 18:50:36 -0700

    commit

--- chamber/scripts/displayMember.js
+++ chamber/scripts/displayMember.js
@@ -6,7 +6,17 @@ async function fetchMemberData() {
     const members = await response.json();
     //console.log(members);
     members.forEach(member => {
-        
+        const memberdiv = document.createElement('div');
+        memberdiv.classList.add('member-card');
+        memberdiv.innerHTML = `
+        <h3>${member.name}</h3>
+        <p>${member.address}</p>
+        <p>${member.phone}</p>
+        <a href="${member.website}" target="_blank">${member.website}</a>
+        <img src="images/${member.image}" alt="${member.name} logo">
+
+        `;
+        membersDiv.append(memberdiv);
     })
 
 }
